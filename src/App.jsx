b710657446b26ad191ef4e7ff4e331ca59308b78
Rahul Vishwakarma <rahulvishwakarmakzm@gmail.com>
2025-07-13 19:13:46 +0530

    add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-// import React from 'react'
-
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Story from "./components/Story"
 
 const App = () => {
   return (
@@ -12,6 +13,9 @@ const App = () => {
      <Hero/>
      <About/>
       <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
     </main>
   )
 }
